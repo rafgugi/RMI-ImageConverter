a10ef0fe18caa0f3b7afd3e04b585fca0116df94
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-03-19 10:10:23 +0800

    handle failed processor. nambah fungsi toString

--- imgClient/src/client/Processor.java
+++ imgClient/src/client/Processor.java
@@ -20,19 +20,29 @@ public class Processor {
     private Registry registry;
     private ImageInterface image;
     public boolean isReady;
+    private String server;
+    private int port;
 
     public Processor(String server, int port) {
         isReady = true;
+        this.server = server;
+        this.port = port;
         try {
             registry = LocateRegistry.getRegistry(server, port);
             image = (ImageInterface) registry.lookup("Image");
         } catch (RemoteException | NotBoundException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println(this + "> " + ex.getMessage());
+            isReady = false;
         }
     }
-    
+
     public ImageInterface getImage() {
         return image;
     }
-    
+
+    @Override
+    public String toString() {
+        return server + ":" + port;
+    }
+
 }
