078e85f85afdc818a152c9780668f9cbc703b6e0
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-03-19 10:08:17 +0800

    nambah fungsi convert time

--- imgClient/src/client/Helper.java
+++ imgClient/src/client/Helper.java
@@ -17,6 +17,14 @@ public class Helper {
     public static final String FOLDER_SRC = "D:/Pictures/src/";
     public static final String FOLDER_DST = "D:/Pictures/dst/";
 
+    public static final FileFilter imageFilter = new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+            String ext = getExtension(file);
+            return ("png".equals(ext) || "jpg".equals(ext));
+        }
+    };
+
     public static String getExtension(File file) {
         String extension = "";
         int j = file.getName().lastIndexOf('.');
@@ -26,11 +34,22 @@ public class Helper {
         return extension;
     }
 
-    public static final FileFilter imageFilter = new FileFilter() {
-        @Override
-        public boolean accept(File file) {
-            String ext = getExtension(file);
-            return ("png".equals(ext) || "jpg".equals(ext));
+    public static String etaConvert(long l_d) {
+        String eta = "";
+        l_d /= 1000;
+        if (l_d >= 86400) {
+            eta += String.valueOf(l_d / 86400) + "d ";
+            l_d %= 86400;
         }
-    };
+        if (l_d >= 3600) {
+            eta += String.valueOf(l_d / 3600) + "h ";
+            l_d %= 3600;
+        }
+        if (l_d >= 60) {
+            eta += String.valueOf(l_d / 60) + "m ";
+            l_d %= 60;
+        }
+        eta += String.valueOf(l_d) + "s";
+        return eta;
+    }
 }
