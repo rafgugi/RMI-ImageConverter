52f16e890cd377b2d3eaedc41db297c4c7e5139e
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-10-10 14:18:10 +0700

    apa?

--- imgClient/src/client/MainClient.java
+++ imgClient/src/client/MainClient.java
@@ -11,8 +11,8 @@ public class MainClient {
 
     public static void main(String[] args) {
         ArrayList<Processor> servers = new ArrayList<>();
-        servers.add(new Processor("127.0.0.1", 22003));
-        servers.add(new Processor("10.151.36.25", 22001));
+        servers.add(new Processor("127.0.0.1", 22001));
+        servers.add(new Processor("10.151.43.104", 22003));
 //        servers.add(new Processor("10.181.1.183", 22003));
         
         Balancer balance = new Balancer(servers);
